41cefe98708dc25f0c27b147c8fa6c805482ed43
Dan Gudmundsson <dgud@erlang.org>
2012-08-17 14:18:38 +0200

    Use Stefan's glsl directly

--- perlin_noise/gl_noise.erl
+++ perlin_noise/gl_noise.erl
@@ -1,182 +1,145 @@
-%%%-------------------------------------------------------------------
-%%% File    : gl_noise.erl
-%%% Author  : Dan Gudmundsson <dgud@erlang.org>
-%%% Description : GLSL Perlin (simplex) noise.
-%%%       This a port of Stefan Gustavson (stegu@itn.liu.se) GLSL-noise code.
-%%%
-%%% Created : 28 Jan 2010 by Dan Gudmundsson <dgud@erlang.org>
-%%%-------------------------------------------------------------------
+%%% @author Dan Gudmundsson <dgud@erlang.org>
+%%% @copyright (C) 2010, Dan Gudmundsson
+%%% @doc GLSL Perlin (simplex) noise.
+%%%       This a port of Stefan Gustavson (stegu@itn.liu.se) GLSL-noise code
+%%%       with some additions for creating textures and other tools. 
+%%% @end
+%%% Created :  2 Feb 2010 by Dan Gudmundsson <dgud@erlang.org>
+
 -module(gl_noise).
--compile(export_all).
+-export([create_tex2D/3]).
+
+%% -compile(export_all). %Debug
 -include_lib("wx/include/gl.hrl").
 
-%%% Creates the two needed tables as binaries.
-init() ->
-    PermTable = create_table(fun permTexture/3),
-    GradTable = create_table(fun gradTexture/3),
-    [PermTable, GradTable].
+%%--------------------------------------------------------------------
+%% @doc  Create a perlin noise 2D texture.
+%%   Creates a tileable 2Dtexture with the given dimensions Width and Heigth
+%%   Starts with on Octave level of noise. 1 small noise ~ 32 very noise.
+%%   Returns a texture id which should be used by gl:bindTexture(Id).
+%% @spec (::integer(), ::integer(), ::integer(), ::fun()) -> TextureId::integer()
+%% @end
+%%--------------------------------------------------------------------
+create_tex2D(W, H, Octave) 
+  when W > 0, H > 0, Octave > 0 ->
+    [Fbo] = gl:genFramebuffers(1),
+    [Col] = gl:genTextures(1),
+    [PX,PY,PW,PH|_] = gl:getIntegerv(?GL_VIEWPORT),
+    setup_fbo(Fbo, Col, W, H),    
 
-%%%
-%%% Initilizes and sets up the needed texture units.
-%%% init_gl(Shader, TexUnit1::integer(), TexUnit2::integer()) -> 
-%%%       {ActivateFun(), DestroyFun()}.
-%%%  Returns two fun's:
-%%%    The ActivateFun/0 should be called before the program is used
-%%%    and the DestroyFun/0 is called to cleanup and delete all textures. 
-%%%  Leaves Shader active afterwards.
-init_gl(Prog, TexUnit1, TexUnit2) ->
-    init_gl(init(), Prog, TexUnit1, TexUnit2).
-init_gl([PermTable, GradTable], Prog, TexUnit1, TexUnit2) ->
-    Ids  = [PermId, GradId] = gl:genTextures(2),
-    Load = fun(Id, Bin) -> 
-		   gl:bindTexture(?GL_TEXTURE_2D, Id),
-		   gl:texImage2D(?GL_TEXTURE_2D, 0, ?GL_RGBA, 256, 256, 
-				 0, ?GL_RGBA, ?GL_UNSIGNED_BYTE, Bin),
-		   gl:texParameteri(?GL_TEXTURE_2D, ?GL_TEXTURE_MIN_FILTER, ?GL_NEAREST),
-		   gl:texParameteri(?GL_TEXTURE_2D, ?GL_TEXTURE_MAG_FILTER, ?GL_NEAREST)
-	   end,
-    Load(PermId,PermTable),
-    Load(GradId,GradTable),
-    
-    gl:useProgram(Prog),
-    PermLoc = gl:getUniformLocation(Prog, "permTexture"),
-    GradLoc = gl:getUniformLocation(Prog, "gradTexture"),
-    
-    gl:uniform1i(PermLoc, TexUnit1),
-    gl:uniform1i(GradLoc, TexUnit2), 
-    
-    {fun() ->  %% Activate Units
-	     gl:activeTexture(?GL_TEXTURE0 + TexUnit1),
-	     gl:bindTexture(?GL_TEXTURE_2D, PermId), 
-	     gl:activeTexture(?GL_TEXTURE0 + TexUnit2),
-	     gl:bindTexture(?GL_TEXTURE_2D, GradId)
-     end,
-     fun() ->  %% Cleanup everything
-	     gl:deleteTextures(Ids)
-     end}.
-		 
-%% Looping noise 
-%% Given that F is your function that generates a real
-%% number from a point in noise-space, and you want your animation to
-%% repeat every t units, you define a new function that loops when z
-%% is between 0 and t:
+    Shader = setup_shader_2D(Octave, W, H),
+    gl:viewport(0,0,W,H),
+    gl:matrixMode(?GL_PROJECTION),
+    gl:pushMatrix(),
+    gl:loadIdentity(),
+    glu:ortho2D(0, W, 0, H),
+    gl:matrixMode(?GL_MODELVIEW),
+    gl:pushMatrix(),
+    gl:loadIdentity(),
+    gl:useProgram(Shader),
 
-%% Floop(x, y, z) = ( (t - z) * F(x, y, z) + (z) * F(x, y, z - t) ) / (t) 
+    gl:'begin'(?GL_QUADS),
+    gl:texCoord2f(0,0), gl:vertex2i(0,0),
+    gl:texCoord2f(1.0,0), gl:vertex2i(W,0),
+    gl:texCoord2f(1.0,1.0), gl:vertex2i(W,H),
+    gl:texCoord2f(0,1.0), gl:vertex2i(0,H),
+    gl:'end'(),
 
-%%% Creating tiling textures
-
-%% Say you're using a noise function to generate textures for a
-%% polygon-based engine. Chances are, you might want the textures to
-%% be tileable. You can extend the above example to generate tileable
-%% 2-dimensional noise that repeats every w units in the x dimension
-%% and every h units in the y dimension. This will require a weighted
-%% sum of four calls to the original function (which again, for lack
-%% of imagination, we will call F).
-
-%%     Ftileable(x, y) = (
-%% 	       F(x, y) * (w - x) * (h - y) +
-%% 		   F(x - w, y) * (x) * (h - y) +
-%% 		   F(x - w, y - h) * (x) * (y) +
-%% 		   F(x, y - h) * (w - x) * (y)
-%% 	      ) / (wh) 
-
-%% A pattern should be emerging here, so it shouldn't surprise you
-%% that you'll need evaluate a weighted sum of 8 calls to the noise
-%% function if you want to have a repeating animation that tiles in
-%% the x and y dimenstions. I'm not going to write it out here out of
-%% space considerations, and also because it should be evident what
-%% the sum will be.
+    gl:flush(),
+    gl:useProgram(0),
+    gl:bindFramebuffer(?GL_FRAMEBUFFER_EXT, 0),
+    gl:deleteFramebuffers([Fbo]),
+    gl:matrixMode(?GL_PROJECTION),
+    gl:popMatrix(),
+    gl:matrixMode(?GL_MODELVIEW),
+    gl:popMatrix(),
+    gl:viewport(PX,PY,PW,PH),
+    Col.
 
 
-%%%%%%%%%%%%%% Implementation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Texture generation helpers
 
-%% Perlins random numbers
--define(PERM_D, 
-	{151,160,137,91,90,15,
-	 131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-	 190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-	 88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-	 77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-	 102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-	 135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-	 5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-	 223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-	 129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-	 251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-	 49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-	 138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180}).
+setup_fbo(Fbo, Col, W, H) ->
+    gl:bindFramebuffer(?GL_FRAMEBUFFER_EXT, Fbo),
+    gl:bindTexture(?GL_TEXTURE_2D, Col), 
+    gl:texImage2D(?GL_TEXTURE_2D, 0, ?GL_RGBA8, W, H, 0, ?GL_RGBA, ?GL_UNSIGNED_BYTE, 0),
+    gl:texParameteri(?GL_TEXTURE_2D, ?GL_TEXTURE_MIN_FILTER, ?GL_LINEAR),
+    gl:texParameteri(?GL_TEXTURE_2D, ?GL_TEXTURE_MAG_FILTER, ?GL_LINEAR),
+    gl:texParameterf(?GL_TEXTURE_2D, ?GL_TEXTURE_WRAP_S, ?GL_REPEAT),
+    gl:texParameterf(?GL_TEXTURE_2D, ?GL_TEXTURE_WRAP_T, ?GL_REPEAT),
 
--define(PERM(V), element((V)+1, ?PERM_D)).
+    gl:framebufferTexture2D(?GL_FRAMEBUFFER_EXT, ?GL_COLOR_ATTACHMENT0_EXT,
+			    ?GL_TEXTURE_2D, Col, 0),
 
-%% These are Ken Perlin's proposed gradients for 3D noise. I kept them for
-%% better consistency with the reference implementation, but there is really
-%% no need to pad this to 16 gradients for this particular implementation.
-%% If only the "proper" first 12 gradients are used, they can be extracted
-%% from the grad4[][] array: grad3[i][j] == grad4[i*2][j], 0<=i<=11, j=0,1,2
+    %% Assert
+    case gl:checkFramebufferStatus(?GL_FRAMEBUFFER_EXT) of
+	?GL_FRAMEBUFFER_COMPLETE_EXT -> ok;
+	Error ->
+	    gl:bindFramebuffer(?GL_FRAMEBUFFER_EXT, 0),
+	    gl:deleteTextures([Col]),
+	    gl:deleteFramebuffers([Fbo]),
+	    exit({error, {frameBufferStatus, Error}})
+    end,
+    ok.
 
--define(GRAD3_D, 
-	{{0,1,1},{0,1,-1},{0,-1,1},{0,-1,-1},
-	 {1,0,1},{1,0,-1},{-1,0,1},{-1,0,-1},
-	 {1,1,0},{1,-1,0},{-1,1,0},{-1,-1,0}, %% 12 cube edges
-	 {1,0,-1},{-1,0,-1},{0,-1,1},{0,1,1}}).
+setup_shader_2D(Scale, W, H) ->
+    Dir = filename:dirname(code:which(?MODULE)),
+    {ok, FsBin0} = file:read_file(filename:join(Dir, "classicnoise2D.glsl")),
+    Main = list_to_binary(io_lib:format(create_tex2d_str(), [W, H,Scale])),
+    FsBin = <<FsBin0/binary, Main/binary>>,
+    VS = gl:createShader(?GL_VERTEX_SHADER),
+    FS = gl:createShader(?GL_FRAGMENT_SHADER),
+    gl:shaderSource(FS, [FsBin]),
+    gl:shaderSource(VS, [vertex_shader()]),
+    gl:compileShader(FS),
+    gl:compileShader(VS),
+    Print = fun(Sh) ->
+		    case gl:getShaderiv(Sh, ?GL_INFO_LOG_LENGTH) of
+			Len when Len > 1 ->
+			    Log = gl:getShaderInfoLog(Sh, Len),
+			    io:format("  Message: ~s~n",[Log]);
+			_ ->
+			    ok
+		    end
+	    end,
+    Print(FS), Print(VS),
 
--define(GRAD3(X,Y), (element(Y, element((X),?GRAD3_D)))).
+    Prog = gl:createProgram(),
+    gl:attachShader(Prog, FS),
+    gl:attachShader(Prog, VS),
+    gl:linkProgram(Prog),
+    case gl:getProgramiv(Prog, ?GL_INFO_LOG_LENGTH) of	
+	Length when Length > 1 ->
+	    Info = gl:getProgramInfoLog(Prog,Length),
+	    io:format("  ~s: ~s~n",["GLSLPerlin", Info]);
+	_ ->  ok
+    end,
+    Prog.
 
-%% These are my (Stefans) own proposed gradients for 4D noise. They are the coordinates
-%% of the midpoints of each of the 32 edges of a tesseract, just like the 3D
-%% noise gradients are the midpoints of the 12 edges of a cube.
+create_tex2d_str() ->
+    "
+varying vec2 texCoord2D;
 
--define(GRAD4_D, 
-	{{0,1,1,1}, {0,1,1,-1}, {0,1,-1,1}, {0,1,-1,-1}, %% 32 tesseract edges
-	 {0,-1,1,1}, {0,-1,1,-1}, {0,-1,-1,1}, {0,-1,-1,-1},
-	 {1,0,1,1}, {1,0,1,-1}, {1,0,-1,1}, {1,0,-1,-1},
-	 {-1,0,1,1}, {-1,0,1,-1}, {-1,0,-1,1}, {-1,0,-1,-1},
-	 {1,1,0,1}, {1,1,0,-1}, {1,-1,0,1}, {1,-1,0,-1},
-	 {-1,1,0,1}, {-1,1,0,-1}, {-1,-1,0,1}, {-1,-1,0,-1},
-	 {1,1,1,0}, {1,1,-1,0}, {1,-1,1,0}, {1,-1,-1,0},
-	 {-1,1,1,0}, {-1,1,-1,0}, {-1,-1,1,0}, {-1,-1,-1,0}}).
+void main( void )\n
+{\n
 
--define(GRAD4(X,Y), (element(Y, element((X),?GRAD4_D)))).
+ float w = ~p.0, h = ~p.0;
+ vec2 pos = vec2(texCoord2D.x*w, texCoord2D.y*h);
+ float o = ~p.0;
+ float div = 1.0/o;
+ float res = pnoise(pos*div, vec2(w/o, h/o));
 
-create_table(Fun) ->
-    Pixels = for_256(Fun, 0,0,array:new(256*256)),
-    array:foldl(fun(_,V,Acc) -> <<Acc/binary, V:32>> end, <<>>, Pixels).
+ gl_FragColor = vec4(0.5+0.5*vec3(res, res, res), 1.0);
 
-%% permTexture - create and load a 2D texture for
-%% a combined index permutation and gradient lookup table.
-%% This texture is used for 2D and 3D noise, both classic and simplex.
+}".
 
-permTexture(I,J,A) ->
-    Offset = I*256+J,
-    Value  = ?PERM((J+?PERM(I)) band 16#FF),
-    V1 = (Value band 16#0F) +1,
-    P1 = ?GRAD3(V1,1) * 64+64,
-    P2 = ?GRAD3(V1,2) * 64+64,
-    P3 = ?GRAD3(V1,3) * 64+64,
-    P4 = Value,
-    P = (P1 bsl 24) bor (P2 bsl 16) bor (P3 bsl 8) bor P4,
-    array:set(Offset, P, A).
-
-gradTexture(I,J,A) ->
-    Offset = I*256+J,
-    Value  = ?PERM((J+?PERM(I)) band 16#FF),
-    V1 = (Value band 16#1F) +1,
-    P1 = ?GRAD4(V1,1) * 64+64,
-    P2 = ?GRAD4(V1,2) * 64+64,
-    P3 = ?GRAD4(V1,3) * 64+64,
-    P4 = ?GRAD4(V1,4) * 64+64,
-    P = (P1 bsl 24) bor (P2 bsl 16) bor (P3 bsl 8) bor P4,
-    array:set(Offset, P, A).
-
-
-%%%%%%%
-
-for_256(Fun,I,256,Acc) ->
-    case I of
-	255 -> Acc;
-	_ -> for_256(Fun,I+1,0,Acc)
-    end;
-for_256(Fun,I,J,A) ->
-    for_256(Fun,I,J+1, Fun(I,J,A)).
-
-
+vertex_shader() ->
+    "
+varying vec2 texCoord2D;
+void main(void) 
+{
+  texCoord2D = gl_MultiTexCoord0.xy;
+  gl_Position = ftransform();
+}
+".
--- perlin_noise/test.erl
+++ perlin_noise/test.erl
@@ -18,21 +18,19 @@
 	       start =erlang:now(),
 	       fcst  =erlang:now()}).  % frame counter start time
 
--record(s,  {frame, canvas, shader, draw, time}).
-
+-record(s,  {frame, canvas, shader, draw, time, active=4}).
 
 -define(W, 800).
 -define(H, 600).
 
--define(SHADERS_MENU, 200).
--define(SHADOW_MENU,  201).
+-define(NOISE_SHADER, 200).
 
 -define(F32, 32/float-native).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 go() ->
-    start().
+    spawn_link(fun() -> start() end).
 
 start() ->
     State0 = initg(),
@@ -43,19 +41,31 @@ start() ->
     ok.
 
 loop(quit) ->    ok;
-loop(State=#s{frame=F, canvas=Canvas, draw=Draw, time=T}) ->
+loop(State=#s{frame=F, canvas=Canvas, draw=Draw, time=T, shader=Shs, active=Active}) ->
     gl:clear(?GL_COLOR_BUFFER_BIT bor ?GL_DEPTH_BUFFER_BIT),
-    gl:matrixMode(?GL_PROJECTION),
-    gl:loadIdentity(),
-    glu:perspective(40, ?W/?H, 0.1, 1000),
-    gl:matrixMode(?GL_MODELVIEW),
-    gl:loadIdentity(),
-    glu:lookAt(0.0,0.0,10.0, 0.0,0.0,0.0, 0.0,1.0,0.0),
-    Draw(T#time.diff, foo),
+    Shader = lists:nth(Active, Shs),
+    case Shader of 
+	{_,2,_} ->
+	    gl:matrixMode(?GL_PROJECTION),
+	    gl:loadIdentity(),
+	    glu:ortho2D(0,?W,0,?H),
+	    gl:matrixMode(?GL_MODELVIEW),
+	    gl:loadIdentity(),
+	    ok;
+	{_,3,_} ->
+	    gl:matrixMode(?GL_PROJECTION),
+	    gl:loadIdentity(),
+	    glu:perspective(40, ?W/?H, 0.1, 1000),
+	    gl:matrixMode(?GL_MODELVIEW),
+	    gl:loadIdentity(),
+	    glu:lookAt(0.0,0.0,10.0, 0.0,0.0,0.0, 0.0,1.0,0.0)
+    end,
+    Draw(T#time.diff, lists:nth(Active, Shs)),
     Ns = events(State#s{time=fps(F,T)}),
     wxGLCanvas:swapBuffers(Canvas),
     %% Sync command queue, so we don't choke the driver and get no events 
     _ = wxWindow:getSize(F),
+    timer:sleep(10),
     loop(Ns).
 
 events(S=#s{frame=Frame}) ->
@@ -67,10 +77,14 @@ events(S=#s{frame=Frame}) ->
 	#wx{id=?wxID_ABOUT} ->
 	    about_box(Frame),
 	    S;
-	#wx{id=?SHADERS_MENU} ->	    
-	    %% RS = S#s.rstate,
-	    %% S#s{rstate= RS#rs{mat_shader = not RS#rs.mat_shader}};
-	    S;
+	#wx{id=What} when What >= ?NOISE_SHADER -> 
+	    Active = What - ?NOISE_SHADER,
+	    try 
+		lists:nth(Active, options()),
+		S#s{active=Active}
+	    catch _:_ ->
+		    S
+	    end;
 	{'_wxe_error_', _, _} -> 
 	    %% Canvas is closed
 	    quit;
@@ -81,12 +95,21 @@ events(S=#s{frame=Frame}) ->
 	    S
     end.
 
+options() ->
+    [
+     {classicnoise2D,  2, "float n = cnoise(v_texCoord2D.xy * 16.0 + sin(0.5 * time));" },
+     {noise2D,         2, "float n = snoise(v_texCoord2D.xy * 16.0 + sin(0.5 * time));"},
+     {classicnoise3D,  3, "float n = cnoise(vec3(4.0 * v_texCoord3D.xyz * (1.0 + sin(0.25 * time))));"},
+     {noise3D,         3, "float n = snoise(vec3(2.0 * v_texCoord3D.xyz * (1.0 + sin(0.25 * time))));"},
+     {classicnoise4D,  3, "float n = cnoise(vec4(8.0 * v_texCoord3D.xyz, 0.5 * time));"},
+     {noise4D,         3, "float n = snoise(vec4(4.0 * v_texCoord3D.xyz, 0.5 * time));"}
+    ].
+
 init_shader(S) ->
     Dir = filename:dirname(code:which(?MODULE)),
     {ok, VsBin} = file:read_file(filename:join(Dir, "noise_test.vs")),
-    {ok, FsBin0} = file:read_file(filename:join(Dir, "glsl_noise.fs")),
 
-    Main = <<"
+    Main0 = "
      uniform float time; // Used for texture animation
      /*
       * Both 2D and 3D texture coordinates are defined, for testing purposes.
@@ -97,41 +120,43 @@ init_shader(S) ->
 
      void main( void )
      {
-      //float n = noise(v_texCoord2D * 32.0 + 240.0);
-      //float n = snoise(v_texCoord2D * 16.0);
-      //float n = noise(vec3(4.0 * v_texCoord3D.xyz * (2.0 + sin(0.25 * time))));
-      float n = snoise(vec3(2.0 * v_texCoord3D.xyz * (2.0 + sin(0.10 * time))));
-      //float n = noise(vec4(8.0 * v_texCoord3D.xyz, 0.5 * time));
-      //float n = snoise(vec4(4.0 * v_texCoord3D.xyz, 0.5 * time));
-      gl_FragColor = v_color * vec4(0.5 + 0.5 * vec3(n, n, n), 1.0);
+       ~s
+       gl_FragColor = v_color * vec4(0.5 + 0.5 * vec3(n, n, n), 1.0);
      }
-    ">>,
-    FsBin = <<FsBin0/binary, Main/binary>>,
-    VS = gl:createShader(?GL_VERTEX_SHADER),
-    FS = gl:createShader(?GL_FRAGMENT_SHADER),
+    ",
     
-    gl:shaderSource(VS, [VsBin]),
-    gl:shaderSource(FS, [FsBin]),
-    
-    gl:compileShader(VS),
-    gl:compileShader(FS),
-    printShaderInfoLog("vertex_shader", VS),
-    printShaderInfoLog("fragment_shader", FS),
-    
-    Prog = gl:createProgram(),
-    gl:attachShader(Prog, VS),
-    gl:attachShader(Prog, FS),
-
-    gl:linkProgram(Prog),
-
-    case gl:getProgramiv(Prog, ?GL_INFO_LOG_LENGTH) of	
-	Length when Length > 1 ->
-	    Info = gl:getProgramInfoLog(Prog,Length),
-	    io:format("  ~s: ~s~n",["GLSLPerlin", Info]);
-	_ ->  ok
-    end,
-    
-    S#s{shader=Prog}.
+    Create = fun({Name, Type, Str}) ->
+		     Main = io_lib:format(Main0, [Str]),
+		     File = filename:join(Dir, atom_to_list(Name) ++ ".glsl"),
+		     io:format("Reading file ~p~n",[File]),
+		     {ok, FsBin0} = file:read_file(File),
+		     FsBin = <<FsBin0/binary, (list_to_binary(Main))/binary>>,
+		     VS = gl:createShader(?GL_VERTEX_SHADER),
+		     FS = gl:createShader(?GL_FRAGMENT_SHADER),
+		     
+		     gl:shaderSource(VS, [VsBin]),
+		     gl:shaderSource(FS, [FsBin]),
+		     
+		     gl:compileShader(VS),
+		     gl:compileShader(FS),
+		     printShaderInfoLog("vertex_shader", VS),
+		     printShaderInfoLog("fragment_shader", FS),
+		     
+		     Prog = gl:createProgram(),
+		     gl:attachShader(Prog, VS),
+		     gl:attachShader(Prog, FS),
+		     
+		     gl:linkProgram(Prog),
+		     
+		     case gl:getProgramiv(Prog, ?GL_INFO_LOG_LENGTH) of	
+			 Length when Length > 1 ->
+			     Info = gl:getProgramInfoLog(Prog,Length),
+			     io:format("  ~s: ~s~n",["GLSLPerlin", Info]);
+			 _ ->  ok
+		     end,
+		     {Name, Type, Prog}
+	     end,
+    S#s{shader=lists:map(Create, options())}.
 
 printShaderInfoLog(File, Shader) ->
     Length  = gl:getShaderiv(Shader, ?GL_INFO_LOG_LENGTH),
@@ -154,12 +179,11 @@ initg() ->
     OptsM    = wxMenu:new([]),
     HelpM    = wxMenu:new([]),    
     wxMenu:append(FileM, ?wxID_EXIT, "&Quit"),    
-    wxMenu:append(OptsM, ?SHADERS_MENU, "&Shaders", 
-		  [{kind, ?wxITEM_CHECK}, 
-		   {help, "Enable Material Shaders"}]), 
-    wxMenu:append(OptsM, ?SHADOW_MENU, "&Shadows", 
-		  [{kind, ?wxITEM_CHECK}, 
-		   {help, "Enable Shadows"}]), 
+    lists:foldl(fun({Name, _, _}, Id) ->
+			wxMenu:append(OptsM, Id, atom_to_list(Name)),
+			Id+1
+		end, ?NOISE_SHADER+1, options()),
+			
     wxMenu:append(HelpM, ?wxID_ABOUT,"&About...\tF1"),
     wxMenuBar:append(MenuBar, FileM, "&File"),
     wxMenuBar:append(MenuBar, OptsM, "&Options"),
@@ -190,35 +214,79 @@ initg() ->
     #s{frame=Frame, canvas=Canvas, time=#time{}}.
 
 
-load_sphere(State = #s{shader=Prog}) ->
+load_sphere(State = #s{shader=[{_,_,AProg}|_]}) ->
     {Size, Data, _} = sphere:tris([{subd,5}, {ccw,false}, {binary,true}, {scale,3}]),
     
     [Buff] = gl:genBuffers(1),
     gl:bindBuffer(?GL_ARRAY_BUFFER,Buff),
     gl:bufferData(?GL_ARRAY_BUFFER, size(Data), Data, ?GL_STATIC_DRAW),
     
-    gl:useProgram(Prog),
-    {ActivateGLSL, _Destroy} = gl_noise:init_gl(Prog, 0, 1),
-    TimeLoc = gl:getUniformLocation(Prog, "time"),
+    gl:useProgram(AProg),
+    %% Nothing to do with the example
+%%    test_create_texture(1),
+    test_create_texture(0, 8),
+    test_create_texture(1, 16),
+    test_create_texture(2, 32),
+    test_create_texture(3, 64),
 
-    Draw = fun(Time, _Rs) ->
+    Draw = fun(Time, {_,Type, Prog}) ->
 		   gl:color3ub(150,70,70),
 		   gl:useProgram(Prog),
-		   ActivateGLSL(),
+		   %%ActivateGLSL(),
+		   TimeLoc = gl:getUniformLocation(Prog, "time"),
 		   gl:uniform1f(TimeLoc, Time/1000),
-		   %% Setup draw buffers
-		   gl:bindBuffer(?GL_ARRAY_BUFFER,Buff),
-		   gl:vertexPointer(3, ?GL_FLOAT, 0, 0),
-		   gl:enableClientState(?GL_VERTEX_ARRAY),
-
-		   %% Draw buffer
-		   gl:drawArrays(?GL_TRIANGLES, 0, Size*3),
+		   case Type of 
+		       2 -> 
+			   gl:'begin'(?GL_QUADS),
+			   gl:texCoord2f(0,0), gl:vertex2i(0,0),
+			   gl:texCoord2f(1,0), gl:vertex2i(?W,0),
+			   gl:texCoord2f(1,1), gl:vertex2i(?W,?H),
+			   gl:texCoord2f(0,1), gl:vertex2i(0,?H),
+			   gl:'end'();
+		       3 ->
+			   %% Setup draw buffers
+			   gl:bindBuffer(?GL_ARRAY_BUFFER,Buff),
+			   gl:vertexPointer(3, ?GL_FLOAT, 0, 0),
+			   gl:enableClientState(?GL_VERTEX_ARRAY),
+			   
+			   %% Draw buffer
+			   gl:drawArrays(?GL_TRIANGLES, 0, Size*3),
 		   
-		   %% Disable these draw buffers
-		   gl:bindBuffer(?GL_ARRAY_BUFFER,0),
-		   gl:disableClientState(?GL_VERTEX_ARRAY)
+			   %% Disable these draw buffers
+			   gl:bindBuffer(?GL_ARRAY_BUFFER,0),
+			   gl:disableClientState(?GL_VERTEX_ARRAY)
+		   end
 	   end,
     State#s{draw=Draw}.
+
+test_create_texture(Id0, Scale) ->
+    H = W = 256,
+    TexId = gl_noise:create_tex2D(W,H,Scale),
+    gl:pixelStorei(?GL_PACK_ALIGNMENT, 1),  %% Unnecessary?
+    Mem = wx:create_memory(W*H*4),
+    gl:bindTexture(?GL_TEXTURE_2D, TexId), 
+    gl:getTexImage(?GL_TEXTURE_2D, 0, ?GL_RGBA, ?GL_UNSIGNED_BYTE, Mem),
+    ImageBin0 = wx:get_memory_bin(Mem),
+    ImageBin = << <<R:8, G:8, B:8>> || <<R:8, G:8, B:8, _A:8>> <= ImageBin0 >>,
+    Image = wxImage:new(W,H,ImageBin),
+    Id = Id0 div 2,
+    Frame = wxFrame:new(wx:null(), ?wxID_ANY,
+			"Noise: " ++ integer_to_list(Scale),
+			[{size, {W*3+10, H*3+10}},
+			 {pos, {(W*3+10)*Id*(Id rem 2),
+				(H*3+40)*(Id0 rem 2)}}
+			]),
+    Panel = wxPanel:new(Frame),
+    Paint = fun(_,_) ->
+		    DC=wxPaintDC:new(Panel),
+		    Bmp = wxBitmap:new(Image),
+		    [ [ wxDC:drawBitmap(DC, Bmp, {5+X*W,5+Y*H}) || X <- [0,1,2] ]
+                      || Y <- [0,1,2]],
+		    wxPaintDC:destroy(DC),
+		    wxBitmap:destroy(Bmp)
+	    end,
+    wxFrame:connect(Panel, paint, [{callback, Paint}]),
+    wxFrame:show(Frame).
 
 %%%%%%%%%
 
